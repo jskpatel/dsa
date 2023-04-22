4631cc659cb2e006da7ce3706c4d73720d43b30a
unknown <developersunilpatel@gmail.com>
2023-04-22 20:19:31 +0530

    Revert "Find index from array through input value"

--- findIndexFromArray.js
+++ findIndexFromArray.js
@@ -2,6 +2,8 @@
 
 // FIND INDEX FROM ARRAY THROUGH INPUT VALUE
 
+
+// 1] IT WILL RETURN FIRST INDEX OF TARGET VALUE AND THEN RETURN
 const arr = [9, 5, 8, 0, 2, 4, 6, 5];
 const value = 5;
 
@@ -10,5 +12,23 @@ const findIndexFromArray = (arr, value) => {
     if (arr[i] === value) return i;
   }
   return false;
+};
+console.log(findIndexFromArray(arr, value));
+
+
+
+
+// 2] IT WILL RETURN ALL TARGETED INDEXES FROM ARRAY
+const arr = [9, 5, 8, 0, 8, 2, 9, 4, 9, 6, 5];
+const value = 9;
+
+const findIndexFromArray = (arr, value) => {
+  let index = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === value) {
+      index.push(i);
+    }
+  }
+  return index;
 };
 console.log(findIndexFromArray(arr, value));
